ad32e3329810db0a49ae76cddecf5eb714e2e860
dimash <fagnoud289@gmail.com>
2024-06-11 17:23:23 +0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <main class="background">
+    <div class="flex items-center justify-center h-screen">
+      <Card />
+    </div>
+  </main>
+</template>
+<script setup>
+import Card from "./components/Card.vue"
+if (
+  localStorage.getItem("color-theme") === "dark" ||
+  (!("color-theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  document.documentElement.classList.add("dark")
+} else {
+  document.documentElement.classList.remove("dark")
+}
+</script>
+
+<style scoped>
+.background {
+  background-image: url(https://i.ibb.co.com/wgz08Q5/night.jpg);
+  background-size: cover;
+}
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,60 @@
+<template>
+  <div
+    class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+  >
+    <div class="flex flex-col items-center pb-10 pt-10">
+      <img
+        class="w-24 h-24 mb-3 rounded-full shadow-lg"
+        src="https://i.ibb.co.com/StRNt8x/Goose-goose.jpg"
+        alt="Bonnie image"
+      />
+      <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">
+        Dinmukhamed Nizami
+      </h5>
+      <span class="text-sm text-gray-500 dark:text-gray-400"
+        >Coder/Startuper</span
+      >
+      <div class="flex flex-col items-center mt-4 md:mt-6">
+        <a href="https://github.com/rimochek">
+          <button
+            type="button"
+            class="w-56 text-white bg-[#ffffff] hover:bg-[#e8e8e8]/90 focus:ring-4 focus:outline-none focus:ring-[#1da1f2]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#1da1f2]/55 me-2 mb-2"
+          >
+            <img
+              class="w-6 h-6 me-2"
+              src="../assets/images/githubicon.png"
+              alt=""
+            />
+            <p class="text-black">View Github</p>
+          </button>
+        </a>
+        <a href="https://t.me/lopsun">
+          <button
+            type="button"
+            class="w-56 text-white bg-[#1da1f2] hover:bg-[#1da1f2]/90 focus:ring-4 focus:outline-none focus:ring-[#1da1f2]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#1da1f2]/55 me-2 mb-2"
+          >
+            <img
+              class="w-6 h-6 me-2"
+              src="../assets/images/tgicon.png"
+              alt=""
+            />
+            Connect via Telegram
+          </button>
+        </a>
+        <a href="https://www.linkedin.com/in/rimochek/">
+          <button
+            type="button"
+            class="w-56 text-white bg-[#114b85] hover:bg-[#0f3f70]/90 focus:ring-4 focus:outline-none focus:ring-[#1da1f2]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#1da1f2]/55 me-2 mb-2"
+          >
+            <img
+              class="w-6 h-6 me-2"
+              src="../assets/images/linkedinicon.png"
+              alt=""
+            />
+            Connect via LinkedIn
+          </button>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
